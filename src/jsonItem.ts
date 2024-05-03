7b9b0f0723d8eba5d3ff16b7552e0cc6d15a8fd4
TERA1103 <hiroto.terazono@gmail.com>
2024-05-03 09:55:55 +0900

    up: jsonItem.ts パース処理のメソッド,ソートとグループ化の関数の作成

--- src/jsonItem.ts
+++ src/jsonItem.ts
@@ -8,8 +8,30 @@ class JsonItem {
   }
 
   // keyをパースして読み取るメソッド
-  private parseToKey(textData: string): string[] {
+  public parseToKey(textData: string): string[] {
+    // private parseToKey(textData: string): string[] {
     // TODO: パース処理
+    let textDataline = textData.split("\n");
+    const c = textDataline.length;
+    
+    for (let i = 0; i < c; i++) {
+      let line = textDataline[i];
+      const re = /".*":/gi;
+      const re2 = /\/\//gi;
+      if (line.match(re2)) {
+        continue;
+      }
+      if (line.match(re)) {
+        let key_str = line.match(re);
+        if (key_str) {
+          const key_str2 = key_str[0].slice(1, key_str[0].length - 2);
+          const key_str_split = key_str2.split(".");
+          return key_str_split;
+        }
+      }
+    }
+
+
     return [""];
   }
 
@@ -28,14 +50,26 @@ class JsonItem {
 // キーの辞書順でソートする
 function sortJsonItems(items: JsonItem[]): JsonItem[] {
   // TODO: itemsをソートする処理
-  return items;
+  return items.sort((a, b) => (a.getKey() > b.getKey() ? 1 : -1));
 }
 
 // 第一キーが同じものをグループ化する関数
 // グループ化したものを返す
 function makeItemGroup(items: JsonItem[]): JsonItem[][] {
   // TODO: itemsをグループ化する処理
-  return [items];
+  let c = 0;
+  let keyname = items[0];
+  const groups = [];
+  for (let i = 0; i < items.length; i++) {
+    if (items[i] !== keyname) {
+      groups.push(items.slice(c, i));
+    }
+    c = i;
+    keyname = items[i];
+  }
+  groups.push(items.slice(c, items.length));
+
+  return groups;
 }
 
 export default JsonItem;
