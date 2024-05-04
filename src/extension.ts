786c1f1ee307c8e182f4db5dd53d4c5e485d7b04
Hiroki Ando <hikuohiku@gmail.com>
2024-05-04 14:34:53 +0900

    refactor: apply format

--- src/extension.ts
+++ src/extension.ts
@@ -7,19 +7,18 @@ import beautifier from './beautifier';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "settings-beautify" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "settings-beautify" is now active!');
-
-	for (let command of commands) {
-		// The command has been defined in the package.json file
-		// Now provide the implementation of the command with registerCommand
-		// The commandId parameter must match the command field in package.json
-		beautifier.setCommand(command);
-		let disposable = vscode.commands.registerCommand(command, beautifier.getFunc());
-		context.subscriptions.push(disposable);
-	}
+  for (let command of commands) {
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    beautifier.setCommand(command);
+    let disposable = vscode.commands.registerCommand(command, beautifier.getFunc());
+    context.subscriptions.push(disposable);
+  }
 }
 
 // This method is called when your extension is deactivated
